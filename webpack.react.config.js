640ffc9077af83cc769159e2649b4767d05aa111
Brandi Richardson <brandirichardson@Brandis-Air.attlocal.net>
2021-01-20 17:12:50 -0500

    style-loader

--- webpack.react.config.js
+++ webpack.react.config.js
@@ -13,7 +13,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\css$/, use: 'css-loader'
+        test: /\css$/, use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.(js|ts|tsx|jsx)$/,   
