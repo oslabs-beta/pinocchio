a6f5870f2a53c7702a1d700089a8da66530b53fc
harlan evans <harlanevans5@gmail.com>
2021-01-29 17:10:35 -0700

    bug with monaco shrink after tree toggling, adjust other child flex comps

--- src/components/Monaco/Monaco.tsx
+++ src/components/Monaco/Monaco.tsx
@@ -1,54 +1,60 @@
-import React, {useState, useEffect, useContext} from 'react';
-import MonacoEditor from 'react-monaco-editor';
-import { Header } from '../../assets/stylesheets/styled-components/Global';
-import { FileContext } from '../../providers/FileProvider';
+import React, { useState, useEffect, useContext } from "react";
+import MonacoEditor from "react-monaco-editor";
+import { Header } from "../../assets/stylesheets/styled-components/Global";
+import { FileContext } from "../../providers/FileProvider";
 
 // import { editor } from 'monaco-editor';
-const { remote } = window.require('electron');
-const electronFs = remote.require('fs');
+const { remote } = window.require("electron");
+const electronFs = remote.require("fs");
 
 // STYLES
-import './Monaco.scss'
+import "./Monaco.scss";
 
 const Monaco = () => {
-  const [ grabContents, setGrabContents ] = useState('');
+  const [grabContents, setGrabContents] = useState("");
   const { chosenFile } = useContext(FileContext);
 
   useEffect(() => {
-    grabFileContents(chosenFile) 
+    grabFileContents(chosenFile);
   }, [chosenFile]);
 
   const grabFileContents = (filePath) => {
-    if (filePath.length > 0){
-      setGrabContents(electronFs.readFileSync(filePath, 'utf8'));
+    if (filePath.length > 0) {
+      setGrabContents(electronFs.readFileSync(filePath, "utf8"));
     }
     console.log(grabContents);
-  }
+  };
   // From spearmint component EditorView.jsx
   const options = {
     selectOnLineNumbers: true,
     // allows min vw of wrapped code in editor
-    wordWrap: 'bounded',
+    wordWrap: "bounded",
     // wordWrapColumn: 90,
     autoIndent: true,
     colorDecorators: true,
-    wrappingIndent: 'indent',
+    wrappingIndent: "indent",
+    // should automatically resize between toggle of file tree, 
+    // is not working as intended
     automaticLayout: true,
+    // right side minimap disabled
+    minimap: { enabled: false },
   };
 
   const editorDidMount = (editor) => {
-    console.log('editorDidMount', editor)
+    console.log("editorDidMount", editor);
     // editor.setTheme('light-dark');
     // editor.focus();
   };
 
-
   return (
-    <div id='monacoCont'>
+    <div id="monacoCont">
       <Header>Code Preview</Header>
       <MonacoEditor
         height="75%"
-        width="33vw"
+        // bug with toggling filetree, flex grow works, but
+        // after retoggling tree back to dom, flew shrink does not work
+        // and monaco keeps the 50% flex width from only two child elements in flex
+        width="100%"
         language="javascript"
         theme="light-dark"
         editorDidMount={editorDidMount}
